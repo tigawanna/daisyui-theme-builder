177459fd421d5c01e004e27107db7e6d0f18cf13
tigawanna <denniskinuthiaw@gmail.com>
2024-08-19 18:21:23 +0300

    add theme list component

---
+++ src/components/themes/ThemesList.tsx
@@ -0,0 +1,11 @@
+interface ThemesListProps {
+
+}
+
+export function ThemesList({}:ThemesListProps){
+return (
+ <div className='w-full h-full flex flex-col items-center justify-center'>
+    list of themes
+ </div>
+);
+}

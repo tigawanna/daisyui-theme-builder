219a84ac2279b041b0b1bdccdc8e83a4ca0686d0
tigawanna <denniskinuthiaw@gmail.com>
2024-08-19 18:21:36 +0300

    install chroma-js

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { ThemesList } from "@/components/themes/ThemesList";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -9,5 +10,6 @@ interface HomePageProps {}
 export function HomePage({}: HomePageProps) {
   return <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
     <h1 className="text-9xl font-bold">homepage </h1>
+    <ThemesList/>
   </div>;
 }

f2f1061147cfca0380550fe0be6efdce482c7f21
tigawanna <denniskinuthiaw@gmail.com>
2024-08-21 21:38:10 +0300

    check if is locked before updating defaults

--- src/helpers/daisyui/css-variables.ts
+++ src/helpers/daisyui/css-variables.ts
@@ -40,28 +40,29 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
   }
   if(colors){
   
-    if (colors?.["base-100"]) {
+    if (colors?.["base-100"]&&!colors?.["base-100"]?.locked) {
       updateCSSVariable({ key: colors["base-100"].variable, value: colors["base-100"].value });
     }
 
-    if (colors?.["base-200"]) {
+    if (colors?.["base-200"]&&!colors?.["base-200"]?.locked) {
       updateCSSVariable({ key: colors["base-200"].variable, value: colors["base-200"].value });
     }
 
-    if (colors?.["base-300"]) {
+    if (colors?.["base-300"]&&!colors?.["base-300"]?.locked) {
       updateCSSVariable({ key: colors["base-300"].variable, value: colors["base-300"].value });
     }
 
-    if (colors?.["base-content"]) {
+    if (colors?.["base-content"]&&!colors?.["base-content"]?.locked) {
       updateCSSVariable({ key: colors["base-content"].variable, value: colors["base-content"].value });
     }
   
 
 
-    if (colors?.primary) {
+    if (colors?.primary&&!colors?.primary?.locked) {
+      alert("updating  primary"+colors.primary.value)
       updateCSSVariable({ key: colors.primary.variable, value: colors.primary.value });
     }
-    if (colors?.["primary-content"]) {
+    if (colors?.["primary-content"]&&!colors?.["primary-content"]?.locked) {
       updateCSSVariable({
         key: colors["primary-content"].variable,
         value: colors["primary-content"].value,
@@ -69,10 +70,10 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
     }
   
 
-    if (colors.secondary) {
+    if (colors.secondary&&!colors?.secondary?.locked) {
       updateCSSVariable({ key: colors.secondary.variable, value: colors.secondary.value });
     }
-    if (colors?.["secondary-content"]) {
+    if (colors?.["secondary-content"]&&!colors?.["secondary-content"]?.locked) {
       updateCSSVariable({
         key: colors["secondary-content"].variable,
         value: colors["secondary-content"].value,
@@ -81,10 +82,10 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
   
 
 
-    if (colors?.accent) {
+    if (colors?.accent&&!colors?.accent?.locked) {
       updateCSSVariable({ key: colors.accent.variable, value: colors.accent.value });
     }
-    if (colors?.["accent-content"]) {
+    if (colors?.["accent-content"]&&!colors?.["accent-content"]?.locked) {
       updateCSSVariable({
         key: colors["accent-content"].variable,
         value: colors["accent-content"].value,
@@ -93,10 +94,10 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
   
 
 
-    if (colors?.neutral) {
+    if (colors?.neutral&&!colors?.neutral?.locked) {
       updateCSSVariable({ key: colors.neutral.variable, value: colors.neutral.value });
     }
-    if (colors?.["neutral-content"]) {
+    if (colors?.["neutral-content"]&&!colors?.["neutral-content"]?.locked) {
       updateCSSVariable({
         key: colors["neutral-content"].variable,
         value: colors["neutral-content"].value,
@@ -105,19 +106,19 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
   
 
 
-    if (colors?.info) {
+    if (colors?.info&&!colors?.info?.locked) {
       updateCSSVariable({ key: colors.info.variable, value: colors.info.value });
     }
-    if (colors?.["info-content"]) {
+    if (colors?.["info-content"]&&!colors?.["info-content"]?.locked) {
       updateCSSVariable({ key: colors["info-content"].variable, value: colors["info-content"].value });
     }
   
 
 
-    if (colors?.success) {
+    if (colors?.success&&!colors?.success?.locked) {
       updateCSSVariable({ key: colors.success.variable, value: colors.success.value });
     }
-    if (colors?.["success-content"]) {
+    if (colors?.["success-content"]&&!colors?.["success-content"]?.locked) {
       updateCSSVariable({
         key: colors["success-content"].variable,
         value: colors["success-content"].value,
@@ -126,10 +127,10 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
   
 
 
-    if (colors?.warning) {
+    if (colors?.warning&&!colors?.warning?.locked) {
       updateCSSVariable({ key: colors.warning.variable, value: colors.warning.value });
     }
-    if (colors?.["warning-content"]) {
+    if (colors?.["warning-content"]&&!colors?.["warning-content"]?.locked) {
       updateCSSVariable({
         key: colors["warning-content"].variable,
         value: colors["warning-content"].value,
@@ -137,10 +138,10 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
     }
   
 
-    if (colors?.error) {
+    if (colors?.error&&!colors?.error?.locked) {
       updateCSSVariable({ key: colors.error.variable, value: colors.error.value });
     }
-    if (colors?.["error-content"]) {
+    if (colors?.["error-content"]&&!colors?.["error-content"]?.locked) {
       updateCSSVariable({
         key: colors["error-content"].variable,
         value: colors["error-content"].value,
@@ -150,31 +151,31 @@ export function loadCSSVariablesFromThemeObject({ theme }: { theme: DaisyUITheme
 }
 
   if (curves) {
-    if (curves?.animation_btn) {
+    if (curves?.animation_btn&&!curves?.animation_btn?.locked) {
       updateCSSVariable({ key: curves.animation_btn.variable, value: curves.animation_btn.value });
     }
-    if (curves?.animation_input) {
+    if (curves?.animation_input&&!curves?.animation_input?.locked) {
       updateCSSVariable({
         key: curves.animation_input.variable,
         value: curves.animation_input.value,
       });
     }
-    if (curves?.tab_border) {
+    if (curves?.tab_border&&!curves?.tab_border?.locked) {
       updateCSSVariable({ key: curves.tab_border.variable, value: curves.tab_border.value });
     }
-    if (curves?.tab_radius) {
+    if (curves?.tab_radius&&!curves?.tab_radius?.locked) {
       updateCSSVariable({ key: curves.tab_radius.variable, value: curves.tab_radius.value });
     }
-    if (curves?.border_btn) {
+    if (curves?.border_btn&&!curves?.border_btn?.locked) {
       updateCSSVariable({ key: curves.border_btn.variable, value: curves.border_btn.value });
     }
-    if (curves.rounded_box) {
+    if (curves.rounded_box&&!curves.rounded_box?.locked) {
       updateCSSVariable({ key: curves.rounded_box.variable, value: curves.rounded_box.value });
     }
-    if (curves.rounded_badge) {
+    if (curves.rounded_badge&&!curves.rounded_badge?.locked) {
       updateCSSVariable({ key: curves.rounded_badge.variable, value: curves.rounded_badge.value });
     }
-    if (curves.btn_focus_scale) {
+    if (curves.btn_focus_scale&&!curves.btn_focus_scale?.locked) {
       updateCSSVariable({
         key: curves.btn_focus_scale.variable,
         value: curves.btn_focus_scale.value,

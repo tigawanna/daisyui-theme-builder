a09dbcee071ce368e0e4384fb453d72f16c90e1c
tigawanna <denniskinuthiaw@gmail.com>
2024-08-23 18:20:32 +0300

    flatten searchParams schema

--- src/helpers/daisyui/daisy-ui-schema.ts
+++ src/helpers/daisyui/daisy-ui-schema.ts
@@ -1,226 +1,242 @@
 import { z } from "zod";
 
-export  const daisyUIThemeColorsSearchParamsSchema = z.object({
-  primary: z.object({
-    name: z.literal("primary"),
-    variable: z.literal("--p"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional(),
-  "primary-content": z.object({
-    name: z.literal("primary-content"),
-    variable: z.literal("--pc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  secondary: z.object({
-    name: z.literal("secondary"),
-    variable: z.literal("--s"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "secondary-content": z.object({
-    name: z.literal("secondary-content"),
-    variable: z.literal("--sc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  accent: z.object({
-    name: z.literal("accent"),
-    variable: z.literal("--a"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "accent-content": z.object({
-    name: z.literal("accent-content"),
-    variable: z.literal("--ac"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  neutral: z.object({
-    name: z.literal("neutral"),
-    variable: z.literal("--n"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "neutral-content": z.object({
-    name: z.literal("neutral-content"),
-    variable: z.literal("--nc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "base-100": z.object({
-    name: z.literal("base-100"),
-    variable: z.literal("--b1"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "base-200": z.object({
-    name: z.literal("base-200"),
-    variable: z.literal("--b2"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "base-300": z.object({
-    name: z.literal("base-300"),
-    variable: z.literal("--b3"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "base-content": z.object({
-    name: z.literal("base-content"),
-    variable: z.literal("--bc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  info: z.object({
-    name: z.literal("info"),
-    variable: z.literal("--in"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "info-content": z.object({
-    name: z.literal("info-content"),
-    variable: z.literal("--inc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  success: z.object({
-    name: z.literal("success"),
-    variable: z.literal("--su"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "success-content": z.object({
-    name: z.literal("success-content"),
-    variable: z.literal("--suc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  warning: z.object({
-    name: z.literal("warning"),
-    variable: z.literal("--wa"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "warning-content": z.object({
-    name: z.literal("warning-content"),
-    variable: z.literal("--wac"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  error: z.object({
-    name: z.literal("error"),
-    variable: z.literal("--er"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-  "error-content": z.object({
-    name: z.literal("error-content"),
-    variable: z.literal("--erc"),
-    value: z.string(),
-    locked: z.boolean().optional(),
-  }).optional()
-  ,
-});
-
+export const daisyUIThemeColorsSearchParamsSchema = z.object({});
 
 export const daisyUIThemeSearchParamsSchema = z.object({
   theme_name: z.string().optional(),
-  colors: daisyUIThemeColorsSearchParamsSchema.optional(),
-  curves: z.object({
-    rounded_box: z
-      .object({
-        name: z.literal("rounded-box"),
-        variable: z.literal("--rounded-box"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    rounded_btn: z
-      .object({
-        name: z.literal("rounded-btn"),
-        variable: z.literal("--rounded-btn"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    rounded_badge: z
-      .object({
-        name: z.literal("rounded-badge"),
-        variable: z.literal("--rounded-badge"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
+  primary: z
+    .object({
+      name: z.literal("primary"),
+      variable: z.literal("--p"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "primary-content": z
+    .object({
+      name: z.literal("primary-content"),
+      variable: z.literal("--pc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  secondary: z
+    .object({
+      name: z.literal("secondary"),
+      variable: z.literal("--s"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "secondary-content": z
+    .object({
+      name: z.literal("secondary-content"),
+      variable: z.literal("--sc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  accent: z
+    .object({
+      name: z.literal("accent"),
+      variable: z.literal("--a"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "accent-content": z
+    .object({
+      name: z.literal("accent-content"),
+      variable: z.literal("--ac"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  neutral: z
+    .object({
+      name: z.literal("neutral"),
+      variable: z.literal("--n"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "neutral-content": z
+    .object({
+      name: z.literal("neutral-content"),
+      variable: z.literal("--nc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "base-100": z
+    .object({
+      name: z.literal("base-100"),
+      variable: z.literal("--b1"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "base-200": z
+    .object({
+      name: z.literal("base-200"),
+      variable: z.literal("--b2"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "base-300": z
+    .object({
+      name: z.literal("base-300"),
+      variable: z.literal("--b3"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "base-content": z
+    .object({
+      name: z.literal("base-content"),
+      variable: z.literal("--bc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  info: z
+    .object({
+      name: z.literal("info"),
+      variable: z.literal("--in"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "info-content": z
+    .object({
+      name: z.literal("info-content"),
+      variable: z.literal("--inc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  success: z
+    .object({
+      name: z.literal("success"),
+      variable: z.literal("--su"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "success-content": z
+    .object({
+      name: z.literal("success-content"),
+      variable: z.literal("--suc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  warning: z
+    .object({
+      name: z.literal("warning"),
+      variable: z.literal("--wa"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "warning-content": z
+    .object({
+      name: z.literal("warning-content"),
+      variable: z.literal("--wac"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  error: z
+    .object({
+      name: z.literal("error"),
+      variable: z.literal("--er"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  "error-content": z
+    .object({
+      name: z.literal("error-content"),
+      variable: z.literal("--erc"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  rounded_box: z
+    .object({
+      name: z.literal("rounded-box"),
+      variable: z.literal("--rounded-box"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  rounded_btn: z
+    .object({
+      name: z.literal("rounded-btn"),
+      variable: z.literal("--rounded-btn"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  rounded_badge: z
+    .object({
+      name: z.literal("rounded-badge"),
+      variable: z.literal("--rounded-badge"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
 
-    animation_btn: z
-      .object({
-        name: z.literal("animation-btn"),
-        variable: z.literal("--animation-btn"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    animation_input: z
-      .object({
-        name: z.literal("animation-input"),
-        variable: z.literal("--animation-input"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    btn_focus_scale: z
-      .object({
-        name: z.literal("btn-focus-scale"),
-        variable: z.literal("--btn-focus-scale"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    border_btn: z
-      .object({
-        name: z.literal("border-btn"),
-        variable: z.literal("--border-btn"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    tab_border: z
-      .object({
-        name: z.literal("tab-border"),
-        variable: z.literal("--tab-border"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-    tab_radius: z
-      .object({
-        name: z.literal("tab-radius"),
-        variable: z.literal("--tab-radius"),
-        value: z.string(),
-        locked: z.boolean().optional(),
-      })
-      .optional(),
-  }).optional(),
+  animation_btn: z
+    .object({
+      name: z.literal("animation-btn"),
+      variable: z.literal("--animation-btn"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  animation_input: z
+    .object({
+      name: z.literal("animation-input"),
+      variable: z.literal("--animation-input"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  btn_focus_scale: z
+    .object({
+      name: z.literal("btn-focus-scale"),
+      variable: z.literal("--btn-focus-scale"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  border_btn: z
+    .object({
+      name: z.literal("border-btn"),
+      variable: z.literal("--border-btn"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  tab_border: z
+    .object({
+      name: z.literal("tab-border"),
+      variable: z.literal("--tab-border"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
+  tab_radius: z
+    .object({
+      name: z.literal("tab-radius"),
+      variable: z.literal("--tab-radius"),
+      value: z.string(),
+      locked: z.boolean().optional(),
+    })
+    .optional(),
 });
 
 // "--p": "primary",          // primary color and its values
--- src/routes/twarkui/index.tsx
+++ src/routes/twarkui/index.tsx
@@ -13,10 +13,10 @@ export function index(){
   })
 return (
   <div className="w-full h-full flex flex-col items-center justify-center">
-    <p className="p-[6%]">{JSON.stringify(searchParams.primary, null, 2)}</p>
+    <p className="p-[6%]">{JSON.stringify(searchParams?.primary, null, 2)}</p>
     <div>
-      {searchParams.primary?.primary.value}
-      {searchParams.primary?.['primary-content']?.value}
+      {searchParams.primary.value}
+      {searchParams?.['primary-content']?.value}
     </div>
   </div>
 );

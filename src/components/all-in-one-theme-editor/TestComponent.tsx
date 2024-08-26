67537aadefe6ff7bc1b1078dc58c979e09bcb66e
tigawanna <denniskinuthiawaweru@gmail.com>
2024-08-26 19:30:08 +0300

    uwu

--- src/components/all-in-one-theme-editor/TestComponent.tsx
+++ src/components/all-in-one-theme-editor/TestComponent.tsx
@@ -3,6 +3,7 @@ interface TestComponentProps {
 }
 export function TestComponent({}:TestComponentProps){
 return (
+    // you can hoist this and mainupulate it uing the theme object
   <div style={{ "--p": "69.51% 0.2132 96.61" }} className="w-full h-full flex flex-col items-center justify-center">
     {/* <style>{`--p: 89.51% 0.2132 96.61`}</style> */}
     <div className="bg-primary text-xl p-[5%]">uwu</div>

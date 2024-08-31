78fce2294ace4f8a0123cdbe7fe97abc337342bc
tigawanna <denniskinuthiaw@gmail.com>
2024-08-31 18:32:58 +0300

    uwu

--- src/components/shadcn/ui/alert.tsx
+++ src/components/shadcn/ui/alert.tsx
@@ -1,14 +1,13 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/components/shadcn/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg text-base-content border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-base-content",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "bg-base-100 text-base-content",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
@@ -16,8 +15,8 @@ const alertVariants = cva(
     defaultVariants: {
       variant: "default",
     },
-  }
-)
+  },
+);
 
 const Alert = React.forwardRef<
   HTMLDivElement,

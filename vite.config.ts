4289333f43d56c747ba681adcdd04d3fab888c39
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-10 16:40:27 +0300

    bump version

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,36 @@
 
 import MillionLint from '@million/lint';
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import analyze from "rollup-plugin-analyzer";
 // import { splashScreen } from "vite-plugin-splash-screen";
-
+const ReactCompilerConfig = {
+  /* ... */
+};
 // https://vitejs.dev/config/
-const _plugins = [react(), TanStackRouterVite(), tsconfigPaths(),
-analyze({
+const _plugins = [
+  react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+    },
+  }),
+  ,
+  TanStackRouterVite(),
+  tsconfigPaths(),
+  analyze({
     // highlight the modules with size > 40kb
     filter(moduleObject) {
       return moduleObject.size > 40000;
     },
   }),
-// splashScreen({
-//   splashBg: "#4D3119",
-//   loaderBg: "#D2BA79",
-//   loaderType: "dots",
-//   logoSrc: "logo.svg",
-// }),
+  // splashScreen({
+  //   splashBg: "#4D3119",
+  //   loaderBg: "#D2BA79",
+  //   loaderType: "dots",
+  //   logoSrc: "logo.svg",
+  // }),
 ];
 if(process.env.MILLION_LINT) {
   _plugins.unshift(MillionLint.vite())
